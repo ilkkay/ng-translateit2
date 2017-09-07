fcc51fd10d41fd597cd5f027adb1c6a12c90c59d
ilkkay <iylakoski@gmail.com>
2017-09-07 14:13:12 +0300

    Added Global and app styles

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+// https://stackoverflow.com/questions/36527605/how-to-style-child-components-from-parent-components-css-file
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
