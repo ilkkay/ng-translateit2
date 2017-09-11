88bbddd46fccb769e05018e54601b198a705605d
ilkkay <iylakoski@gmail.com>
2017-09-11 13:33:44 +0300

    2017-01-09 Monday

--- src/app/shared/appconfig.service.ts
+++ src/app/shared/appconfig.service.ts
@@ -3,16 +3,37 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppconfigService {
 
- private productionState: boolean;
+ private detailUrl = '/projects';
+
+ private types = [
+    'ISO8859_1', 'UTF_8'
+  ];
+
+  private formats = [
+    'PROPERTIES', 'XLIFF'
+  ];
+
+  private productionState: boolean;
 
   constructor() { }
 
-  setProductionState (state: boolean) {
+  setProductionState(state: boolean) {
     this.productionState = state;
   }
 
   isInProduction(): boolean {
-    console.log ('Production state is now ' + this.productionState);
+    console.log('Production state is now ' + this.productionState);
     return this.productionState;
   }
+
+  getFormats(): any { return this.formats; }
+
+  getTypes(): any { return this.types; }
+
+  setFormats(formats: any[]): void { this.formats = formats; }
+
+  setTypes(types: any[]): void { this.types = types; }
+
+  getDetailUrl(): string { return this.detailUrl; }
+
 }
