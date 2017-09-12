c6a76197b906e385c82ab1004cff22ff7cb634dc
ilkkay <iylakoski@gmail.com>
2017-09-12 16:25:43 +0300

    2017-09-12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Component, NgModule, VERSION } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsModule } from './projects/projects.module';
 import { ProjectsRoutingModule } from './projects/projects-routing.module';
@@ -10,22 +11,13 @@ import { ProjectDetailComponent } from './projects/project-detail/project-detail
 import { ProjectSearchComponent } from './projects/project-search/project-search.component';
 import { ProjectsmainComponent } from './projects/projectsmain/projectsmain.component';
 import { ProjectListComponent } from './projects/project-list/project-list.component';
-
-import {parent} from '../parent';
-import {child} from '../child';
+import { ErrorMessageComponent } from './shared/error-message/error-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     ProjectsComponent,
-
-    //ProjectDetailComponent,
-    //ProjectSearchComponent,
-    //ProjectsmainComponent,
-    //ProjectListComponent,
-
-    parent,child ,
+    ErrorMessageComponent ,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/error-message/error-message.component.ts
@@ -0,0 +1,44 @@
+import { isEmpty } from 'rxjs/operator/isEmpty';
+import { isUndefined } from 'util';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.css']
+})
+export class ErrorMessageComponent implements OnInit {
+
+  successMessage = '';
+  errorMessage = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setSuccessMessage(successEvent: any) {
+    this.successMessage = successEvent;
+  }
+
+  setErrorMessage(errorEvent: any) {
+    if (errorEvent === '') {
+      this.errorMessage = '';
+    } else {
+      this.errorMessage = this.getErrorMessages(errorEvent);
+    }
+  }
+
+  private getErrorMessages(error: any): string {
+    const obj = JSON.parse(error.text());
+    const errorCode = obj['errorCode'];
+    const errorMessage = obj['errorMessage'];
+    const localizedErrorMessage = obj['localizedErrorMessage'];
+
+    let msg = 'Error Code:' + errorCode + ' ';
+    msg = msg + 'Message: ' + errorMessage[0] + ' ';
+    msg = msg + 'Localized Message:' + localizedErrorMessage + ' ';
+
+    return (msg);
+  };
+}
