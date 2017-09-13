08dee1af5947d2c7acfbe8f02514838829a3fef9
ilkkay <iylakoski@gmail.com>
2017-09-13 12:46:58 +0300

    new master

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+/* Some components (md-slide-toggle, md-slider, mdTooltip) rely on HammerJS
+for gestures. In order to get the full feature-set of these components,
+HammerJS must be loaded into the application.
+*/
+import 'hammerjs';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
