d38c6b3d12acb3824639e5c29c00245aa14079f5
ilkkay <iylakoski@gmail.com>
2017-09-15 14:45:56 +0300

    Intial unit tests for ProjectDetailComponent

--- src/app/projects/project-detail/project-detail.component.spec.ts
+++ src/app/projects/project-detail/project-detail.component.spec.ts
@@ -1,23 +1,92 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import {
+  Http,
+  HttpModule,
+  XHRBackend,
+  ResponseOptions,
+  Response,
+  BaseRequestOptions,
+  RequestOptions,
+  RequestMethod
+} from '@angular/http';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { MdDialogModule, MdButtonModule, MdTooltipModule } from '@angular/material';
+import { MdDialog, MdDialogConfig, MdDialogRef } from '@angular/material';
+import { ConfirmDeleteComponent } from '../../material/mymaterial.module';
+
+import 'rxjs/add/operator/switchMap';
+import {Observable} from 'rxjs/Observable';
+
+import { AppconfigService } from '../../shared/appconfig.service';
 import { ProjectDetailComponent } from './project-detail.component';
+import { ProjectService } from '../shared/project.service';
+import { Project } from '../shared/project';
 
-describe('ProjectDetailComponent', () => {
+class MockActivatedRoute extends ActivatedRoute {
+  public params = Observable.of({id: 123});
+}
+
+fdescribe('ProjectDetailComponent', () => {
   let component: ProjectDetailComponent;
   let fixture: ComponentFixture<ProjectDetailComponent>;
+  let projectDetailComponent: ProjectDetailComponent;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProjectDetailComponent ]
-    })
-    .compileComponents();
-  }));
+  let _projectService: ProjectService;
+  let _appConfig: AppconfigService;
+  let _route: ActivatedRoute;
+  let _router: Router;
+  let _formBuilder: FormBuilder;
+  let _dialog: MdDialog;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ ProjectService, AppconfigService,
+        { provide: ActivatedRoute, useValue: new MockActivatedRoute() },
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, options: BaseRequestOptions) =>
+            new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }],
+      declarations: [ ProjectDetailComponent],
+      imports: [
+        ReactiveFormsModule, FormsModule,
+        MdDialogModule, MdButtonModule, MdTooltipModule,
+        RouterTestingModule
+      ]
+    });
     fixture = TestBed.createComponent(ProjectDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  beforeEach(inject([
+    ProjectService, AppconfigService, ActivatedRoute, Router,
+    FormBuilder, MdDialog ],
+    (
+      __projectService: ProjectService, __appConfig: AppconfigService,
+      __route: ActivatedRoute, __router: Router,
+      __formBuilder: FormBuilder, __dialog: MdDialog
+    ) => {
+        _projectService = __projectService;
+        _appConfig = __appConfig;
+        _route = __route;
+        _router = __router;
+        _formBuilder = __formBuilder;
+        _dialog = __dialog;
+
+        projectDetailComponent = new ProjectDetailComponent(_projectService,
+          _appConfig, _route, _router, _formBuilder, _dialog );
+        }
+    ));
 
   it('should be created', () => {
     expect(component).toBeTruthy();
