f624b9641c910b2b30e1ca591a54b73ae2887c7e
ilkkay <iylakoski@gmail.com>
2017-10-03 15:49:54 +0300

    Updated WorkDetailComponent

--- src/app/works/work-detail/work-detail.component.ts
+++ src/app/works/work-detail/work-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-work-detail',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkDetailComponent implements OnInit {
 
-  constructor() { }
+  priorities = [];
+
+  workForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  registerFormControls(): void {
+    this.workForm = this.formBuilder.group({
+      deadline: [''],
+      id: [],
+      locale: ['', Validators.compose([Validators.required,
+      Validators.pattern('[a-z]{2}_[a-zA-Z]{2}')])],
+      sourceFile: [''],
+      originalFile: [''],
+      priority: [],
+      version: [''],
+    });
+  }
 }
